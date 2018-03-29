302e2a6b36963aafe24b7680db941c3dc7b4628a
Joe Tiersma <snowjava13@gmail.com>
2018-03-28 22:00:46 -0500

    Starting to adapt app for final project. Changing to baseball. Added the background and make team choice spinners.

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.android.courtcounter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +17,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+//    Spinner spinner = (Spinner) findViewById(R.id.spinner);
+//    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+//            R.array.baseball_team_names, android.R.layout.simple_spinner_item);
+//    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//    spinner.setAdapter(adapter);
+//
+//    public class SpinnerActivity extends Activity implements AdapterView.OnItemSelectedListener {
+//
+//    }
 
     public void add3TeamA(View view) {
         scoreTeamA = scoreTeamA + 3;
@@ -54,12 +66,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_A_score);
+        TextView scoreView = findViewById(R.id.team_A_score);
         scoreView.setText(String.valueOf(score));
     }
 
     public void displayForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_B_score);
+        TextView scoreView = findViewById(R.id.team_B_score);
         scoreView.setText(String.valueOf(score));
     }
 

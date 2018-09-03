b6cdd63be6248b47778b1893bc94cd3742b1d73f
joe <snowjava13@gmail.com>
2018-09-03 17:40:41 -0500

    VCS update commit

--- app/src/main/java/com/example/android/courtcounter/CustomAdapter.java
+++ app/src/main/java/com/example/android/courtcounter/CustomAdapter.java
@@ -1,7 +1,6 @@
 package com.example.android.courtcounter;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -11,6 +10,9 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+/**
+ * Created by abdalla on 10/29/17.
+ */
 
 public class CustomAdapter extends ArrayAdapter<String> {
 
